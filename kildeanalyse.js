bbd3d0464bda9e2049393b5f9e832397ddfd82bf
Anders Tornøe <ato@kvuc.dk>
2015-10-06 11:50:11 +0200

    Some tweaks

--- kildeanalyse.js
+++ kildeanalyse.js
@@ -6,8 +6,6 @@ score = 0;
 var hejsa = "";
 
 
-///VIRK SÅ MOTHERFUCKER!
-
 
 $(document).ready(function() {
 
@@ -15,9 +13,9 @@ $(document).ready(function() {
 
 
     $(".dropdown").change(function() {
-        var korrekt_indeks = $(".dropdown option:selected").index(); 
-       
-        if (korrekt_indeks === JsonObj[runde].opts[delrunde].korrekt_svar) {
+        var valgt_indeks = $(".dropdown option:selected").index();
+
+        if (valgt_indeks === JsonObj[runde].opts[delrunde].korrekt_svar) {
             korrekt_svar = true;
         } else {
             korrekt_svar = false;
@@ -29,6 +27,9 @@ $(document).ready(function() {
 
 
 function next_round() {
+
+    $(".faerdig_tekst").fadeOut(0);
+
     $(".analysetext").html("");
     delrunde = 0;
     $(".kilde_container").fadeOut(0).html("").fadeIn(1500);
@@ -43,7 +44,7 @@ function next_round() {
             $(".kilde_container").append(txt_fil)
         });
 
-    } else if (kilde_type == "film") {
+    } else if (kilde_type == "video") {
         $(".kilde_container").append("<iframe class='embed-responsive-item' src='https://www.youtube.com/embed/" + JsonObj[runde].kilde + "'frameborder='0' allowfullscreen='1'></iframe>");
     } else if (kilde_type == "pic") {
         $(".kilde_container").append("<img class='pic' src='" + JsonObj[runde].kilde + "'>");
@@ -56,6 +57,7 @@ function next_del_round() {
     korrekt_svar = false;
     $(".QuestionTask").html("");
     $(".dropdown").html("");
+
     $(".QuestionTask").html(JsonObj[runde].opts[delrunde].spm);
     $(".QuestionTask").fadeOut(0).fadeIn(2000);
     $(".dropdown").append("<option value='Vælg dit svar' selected>Vælg dit svar</option>");
@@ -70,26 +72,41 @@ function check_answer() {
     var dropdown_value = $(".dropdown").val();
 
     if (korrekt_svar === true) {
-        if (delrunde < JsonObj[runde].opts.length) {
-            console.log("næste delrunde_spm");
 
-            $(".analysetext").append("<span class='txt_tween'>" + JsonObj[runde].opts[delrunde].feedback + "</span>");
-            $(".txt_tween").eq(delrunde).fadeOut(0);
-            $(".txt_tween").eq(delrunde).fadeIn('slow', function() {
-                delrunde++;
+        console.log("næste delrunde_spm");
+
+        $(".analysetext").append("<span class='txt_tween'>" + JsonObj[runde].opts[delrunde].feedback + "</span>");
+        $(".txt_tween").eq(delrunde).fadeOut(0);
+        $(".txt_tween").eq(delrunde).fadeIn('slow', function() {
+
+            delrunde++;
+
+            if (delrunde < JsonObj[runde].opts.length) {
                 next_del_round();
-            });
-
-        } else {
-            $(".spm_container").append("<div class='btn btn-default continue'>Næste kilde</div>")
-            runde++;
-            $(".continue").click(next_round);
-        }
+            } else {
+                $(".dropdown").fadeOut(0);
+                $(".checkAnswer").fadeOut(0);
+                $(".QuestionTask").fadeOut(0).html("Du har stillet kilden en mængde spørgsmål og kan læse den samlede kildeanalyse her:").fadeIn(2000);
+                runde++;
+                if (runde < JsonObj.length) {
+                    $(".spm_container").append("<div class='btn btn-primary continue'>Næste kilde</div>");
+                    $(".continue").click(next_round);
+                } else {
+                    $(".spm_container").append("Opgaven er slut. <br/><div class='btn btn-primary again'>Prøv igen</div>");
+                    $(".again").click(function() {
+                        location.reload();
+                    });
+                }
+            }
+        });
         console.log("move on");
         //update_text();
     } else {
-        UserMsgBox(".container-fluid", dropdown_value + " er ikke det korrekte svar.");
-
+        if ($(".dropdown option:selected").index() != 0) {
+            UserMsgBox("html", dropdown_value + " er ikke det korrekte svar.");
+        }else{
+            UserMsgBox("html", "Vælg et svar fra dropdown menuen til venstre");
+        }
     }
 }
 
